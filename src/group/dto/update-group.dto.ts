579a9a822cb464d06e31e5e462c46ba3dd32d764
malek <khannoussimalek@gmail.com>
2024-04-27 17:38:30 +0100

    update dto of group
    Co-authored-by: karim-cherif <abedelcarimc@gmail.com>
    Co-authored-by: khannoussi-malek <khannoussimalek@gmail.com>

--- src/group/dto/update-group.dto.ts
+++ src/group/dto/update-group.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
 import { CreateGroupDto } from './create-group.dto';
 
-export class UpdateGroupDto extends PartialType(CreateGroupDto) {}
+export class UpdateGroupDto extends CreateGroupDto {
+  @IsOptional()
+  @ApiProperty()
+  students?: number[];
+
+  @IsOptional()
+  @ApiProperty()
+  subjects?: number[];
+}
